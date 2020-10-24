edf03eedb994303729e435d0bef9501067a114b2
HuangZonghao <coding@hzh.io>
2020-10-24 18:33:13 -0400

    rearrange project structure, add simulation and electronics test target

--- electronics/src/main.cpp
+++ electronics/src/main.cpp
@@ -2,20 +2,6 @@
 #include "testfile.h"
 
 #include <iostream>
-#include "SimulationManager.h"
-
-const std::string urdf_filename = "../data/robots/fourwheels.urdf";
-// const std::string urdf_filename = "../data/robots/fourleg.urdf";
-const std::string env_filename = "";
-
-const double s_friction = 2.0;
-const double k_friction = 1.9;
-
-// #include <limits>
-
-using namespace std;
-using namespace Eigen;
-
 int main() {
 
 	// READ TEST
@@ -29,39 +15,6 @@ int main() {
 	camera_read();
 	bluetooth_read();
 	encoder_read();
-
-    /*
-    // first init simulation manager
-    SimulationManager sm;
-    sm.SetUrdfFile(urdf_filename);
-    sm.SetEnvFile(env_filename);
-    sm.SetFrictionK(k_friction);
-    sm.SetFrictionS(s_friction);
-
-    // this part will be done by UI
-    sm.AddMotor("chassis_wheel_rl", 1,0.1,0.1,0.1);
-    sm.AddMotor("chassis_wheel_rr", 1,0.1,0.1,0.1);
-    // sm.AddMotor("base_link_link1", 1,0.1,0.1,0.1);
-    // sm.AddMotor("base_link_link2", 1,0.1,0.1,0.1);
-    // sm.AddMotor("base_link_link3", 1,0.1,0.1,0.1);
-    // sm.AddMotor("base_link_link4", 1,0.1,0.1,0.1);
-    sm.AddWaypoint(0, 0, 1);
-    sm.AddWaypoint(5, 0, 1);
-    sm.AddWaypoint(8, 0, 1);
-
-    // use this loop for iterations
-    bool sim_done = false;
-    while (!sim_done){
-        bool task_done = sm.RunSimulation(true);
-        // bool task_done = sm.RunSimulation(false);
-
-        // now the torques are ready to read
-        std::cout << "motor 1 torque " << sm.motors[0]->max_torque << std::endl;
-        std::cout << "motor 2 torque " << sm.motors[1]->max_torque << std::endl;
-
-        sim_done = true;
-    }
-    */
 
 	// SOLVE TEST
 //	motor_solve();
